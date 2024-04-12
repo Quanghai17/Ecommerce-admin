b8b9105821be76d49b3a342912c9216fe29f565d
Hải Nguyễn quang <hainguyenquang@Hais-MacBook-Air.local>
2024-04-12 22:42:40 +0700

    12/4

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,7 +6,9 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -31,8 +33,26 @@ export const StoreModal = () => {
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}>
             <div className="space-y-4 py-2 pb-4">
-                <Form children={undefined} {...form}>
-
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField 
+                        control={form.control} 
+                        name="name" 
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="E-commerce" {...field}/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}>
+                        </FormField>
+                        <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                            <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                            <Button type="submit">Continue</Button>
+                        </div>
+                    </form>
                 </Form>
             </div>
         </Modal>
